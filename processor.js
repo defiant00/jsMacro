4c097a6b032876ffa028639032bbbf5051d24458
Aaron Mady <defiant00@gmail.com>
2017-02-27 18:07:22 -0500

    initial token parsing

--- processor.js
+++ processor.js
@@ -5,15 +5,54 @@ function change() {
 	output.value = process(input.value);
 }
 
+
+
+
 function process(val) {
-	let regex = /[_a-zA-Z][_a-zA-Z0-9]*|[+-]?[0-9]+|[-+*/%^]+|'(?:\\'|[^'])*'/g;
-	let res;
+	let cur = val;
 	let tokens = [];
-	while ((res = regex.exec(val))) {
-		tokens.push('[' + res[0] + ']');
+	let res;
+	while (cur.length > 0) {
+		if (/^\s/.test(cur)) {
+			cur = cur.substring(1);
+		} else if ((res = /^(?:[_a-zA-Z][_a-zA-Z0-9]*|[+-]?[0-9]+(?:\.[0-9]+)?)/.exec(cur))) {
+			tokens.push(res[0]);
+			cur = cur.substring(res[0].length);
+		} else if (cur[0] == '[') {
+			cur = _tokString(cur, tokens);
+		} else {
+			cur = _tokOther(cur, tokens);
+		}
 	}
 
-	return tokens.join(' ');
+	return tokens.map(t => '{' + t + '}').join(' ');
+}
+
+function _tokString(val, tokens) {		// TODO - Replace with token and remaining buffers to strip out the '\' escape char.
+	let depth = 1;
+	let ind = 1;
+	while (ind < val.length && depth > 0) {
+		if (val[ind] === '[') {
+			depth++;
+		} else if (val[ind] === ']') {
+			depth--;
+		} else if (val[ind] === '\\') {
+			ind++;
+		}
+		ind++;
+	}
+	ind = Math.min(ind, val.length);
+	tokens.push(val.substring(1, depth === 0 ? ind - 1 : ind));
+	return val.substring(ind);
+}
+
+function _tokOther(val, tokens) {
+	let ind = 1;
+	while (ind < val.length && !/[\s_a-zA-Z0-9]/.test(val[ind])) {
+		ind++;
+	}
+	tokens.push(val.substring(0, ind));
+	return val.substring(ind);
 }
 
 window.onload = () => {
